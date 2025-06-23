d0f871e209e001ddf1aa69384b1be2a15964e0db
Alankrit121 <al.aggarwal2005@gmail.com>
2025-06-23 17:55:24 +0530

    Update: modified index.html and styles

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/Nintendo_UI/",
   plugins: [react(), tailwindcss()],
   css: {
     postcss: true,
